587bab2af962ae0cbde9e8c0e58beedbfdfeac37
Levan Sakvarelidze <l.sakvarelidze@hotmail.com>
2022-12-03 16:02:51 +0400

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,80 @@
+import { useMultiformSteps } from "./useMultiformSteps"
+import { AccountForm } from "./AccountForm"
+import { AddressForm } from "./AddressForm"
+import { UserForm } from "./UserForm"
+import { FormEvent, useState } from "react"
+
+type FormData = {
+  firstName: string
+  lastName: string
+  age: string
+  street: string
+  city: string
+  state: string
+  zip: string
+  email: string
+  password: string
+}
+
+const INITIAL_DATA: FormData = {
+  firstName: "",
+  lastName: "",
+  age: "",
+  street: "",
+  city: "",
+  state: "",
+  zip: "",
+  email: "",
+  password: "",
+}
+
+function App() {
+  const [data, setData] = useState(INITIAL_DATA)
+
+  function updateFields(fields: Partial<FormData>) {
+    setData(prev => {
+      return { ...prev, ...fields }
+    })
+  }
+
+  const { step, steps, currentStepIndex, next, back, isFirstStep, isLastStep } =
+    useMultiformSteps([
+      <UserForm {...data} updateFields={updateFields} />,
+      <AddressForm {...data} updateFields={updateFields} />,
+      <AccountForm {...data} updateFields={updateFields} />
+    ])
+
+  function onSubmit(e: FormEvent) {
+    e.preventDefault()
+    if (!isLastStep) return next()
+    alert("Successful Account Creation")
+  }
+  return (
+    <div style={{
+      position: "relative",
+      border: "1px solid black",
+      padding: "2rem",
+      margin: "1rem auto",
+      borderRadius: ".5rem",
+      fontFamily: "Arial",
+      maxWidth: "max-content"
+    }}>
+      <form onSubmit={onSubmit}>
+        <div style={{
+          position: "absolute",
+          top: ".5rem",
+          right: ".5rem"
+        }}>
+          {currentStepIndex + 1} / {steps.length}
+        </div>
+        {step}
+        <div style={{ marginTop: "1rem", display: "flex", gap: ".5rem", justifyContent: "flex-end" }}>
+          {!isFirstStep && <button type="button" onClick={back}>Back</button>}
+          <button type="submit">{isLastStep ? "Finish" : "Next"}</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default App
---
+++ src/UserForm.tsx
@@ -0,0 +1,47 @@
+import { FormWrapper } from "./FormWrapper";
+
+type UserData = {
+    firstname: string
+    lastname: string
+    age: string
+}
+
+type UserFormProps= UserData & {
+    updateFields: (fields: Partial<UserData>) => void
+}
+
+
+export function UserForm({
+    firstname,
+    lastname,
+    age,
+    updateFields
+}: UserFormProps) {
+    return (
+        <FormWrapper title="User Form">
+            <label>First Name</label>
+            <input
+                autoFocus
+                required
+                type="text"
+                value={firstname}
+                onChange={e => updateFields({ firstname: e.target.value })}
+            />
+            <label>Last Name</label>
+            <input
+                required
+                type="text"
+                value={lastname}
+                onChange={e => updateFields({ lastname: e.target.value })}
+            />
+            <label>Age</label>
+            <input
+                required
+                min={1}
+                type="number"
+                value={age}
+                onChange={e => updateFields({ age: e.target.value })}
+            />
+        </FormWrapper>
+    )
+}
